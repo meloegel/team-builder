9106ed24d22bfa6584de961221cb5e91d74b8145
meloegel <meloegel@gmail.com>
2020-05-19 19:25:48 -0400

    completed

---
+++ team-builder/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+import App from './components/App.jsx';
+import { render } from 'react-dom'
+
+render(
+  <App />
+  , document.querySelector('#root')
+)
+
